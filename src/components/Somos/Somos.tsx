a483ce1a0f09be0a3f279f427c5b5f87ba7a36be
jorgelomeli98 <jorgelomeli98@hotmail.com>
2025-04-25 12:34:14 -0600

    correccion de rutas, (otra vez)

--- src/components/Somos/Somos.tsx
+++ src/components/Somos/Somos.tsx
@@ -45,7 +45,7 @@ export const Somos = () => {
       <h2 className="titulo-container">Este es nuestro mundo</h2>
       <div className="card-grid">
         <Card
-          srcImg="/public/assets/que-es-dm-admo-ac.jpg"
+          srcImg={`${import.meta.env.BASE_URL}assets/que-es-dm-admo-ac.jpg`}
           text={queEsDistrofia}
         />
         <Card children={videoAdmo} idCssCard="iframe-video" text="¿Que es ADMO?"/>
